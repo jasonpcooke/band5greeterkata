b548dbb504afefeecc47afa87f0101c08dcdb845
Jason Cooke <jasonpcooke@gmail.com>
2025-01-21 18:44:57 +0000

    Greeter-006 Differ greeting depending on time of day- unit test added

--- src/test/java/com/band5/greeter/kata/GreeterTest.java
+++ src/test/java/com/band5/greeter/kata/GreeterTest.java
@@ -2,6 +2,8 @@ package com.band5.greeter.kata;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreeterTest {
@@ -24,5 +26,13 @@ class GreeterTest {
     var greeter = new Greeter();
     String name = "john";
     assertEquals("Hello John", greeter.greet(name));
+  }
+
+  @Test
+  void greetReturnsGoodMorningWhenTimeBetween0600and1200() {
+    var greeter = new Greeter();
+    LocalTime morning = LocalTime.of(6, 0);
+    String name = "John";
+    assertEquals("Good Morning John", greeter.greet(name, morning));
   }
 }
